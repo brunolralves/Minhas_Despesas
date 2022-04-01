b2e079fd02ecb97f2d2df5cc0b4453073ba1d264
brunolralves <brunolralves@yahoo.com>
2022-04-01 02:08:46 -0300

    Iniciando projeto

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, TextInput} from 'react-native';
+
+export default function index() {
+  return (
+    <View>
+      <TextInput>Teste</TextInput>
+    </View>
+  );
+}
